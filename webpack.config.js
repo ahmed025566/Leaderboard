584891ee65c01beedee0b3ea501be2b1ecc5f37f
unknown <ahmedsalah025566%gmail.com>
2023-03-14 20:32:41 +0200

    Upload First version of Leader Board

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    mode: 'development',
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    Functions: './src/Functions.js',
+  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
